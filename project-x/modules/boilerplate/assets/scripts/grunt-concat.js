d3aba1fe4cff0aa07190cb17e9a8e9c55d97a237
Antoine Boulanger <antoineboulangerz@gmail.com>
2015-01-28 23:00:00 -0500

    Add external grunt concat file

---
+++ project-x/modules/boilerplate/assets/scripts/grunt-concat.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+  	main: {
+		src: [
+			 'assets/scripts/src/vendors/jquery-1.11.2.min.js'
+			,'assets/scripts/src/main.js'
+		],
+		dest: 'assets/scripts/dist/main.js',
+	}
+	
+}
