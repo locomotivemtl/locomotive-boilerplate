1ce377e24426f2a3a5c2dd9369b8382062c963c7
Antoine Boulanger <antoineboulangerz@gmail.com>
2015-03-05 17:19:45 -0500

    jQuery CDN & fix wrong paths index.html

--- project-x/modules/boilerplate/assets/templates/boilerplate.inc.footer.php
+++ project-x/modules/boilerplate/assets/templates/boilerplate.inc.footer.php
@@ -1,4 +1,5 @@
-		
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="modules/{{module}}/assets/scripts/src/vendors/jquery-1.11.2.min.js"><\/script>')</script>		
         <!--<script src="modules/{{module}}/assets/scripts/dist/main.js"></script>-->
     </body>
 </html>
