e4e037fc2e577f66e5f9e8aaa26645f3ca095992
Mathieu Ducharme <mat@locomotive.ca>
2015-03-13 17:47:51 -0400

    Minor config fixes

--- project-x/config/config.php
+++ project-x/config/config.php
@@ -7,16 +7,27 @@
 error_reporting(E_ALL & ~E_STRICT);
 ini_set('display_errors', true);
 
-// Environment defaults
-date_default_timezone_set('America/Montreal');
+
 
 // Main Charcoal include. This is where the Charcoal autoloader is defined.
 include __DIR__.'/../charcoal/charcoal.php';
 
 // JSON Configuration
 Charcoal::add_config(__DIR__.'/config.json');
+$application_env = Charcoal::application_env();
+if(file_exists(__DIR__ . '/config.'.$application_env.'.json')) {
+	Charcoal::add_config(__DIR__ . '/config.'.$application_env.'.json');
+}
+
+// Environment defaults
+date_default_timezone_set('America/Montreal');
 
 // Configuration overwrite
 Charcoal::$config['ROOT'] = realpath(__DIR__).'/../';
-Charcoal::$config['URL'] = 'http://'.$_SERVER['HTTP_HOST'].'/';
+if(isset($_SERVER['HTTP_HOST'])) {
+	Charcoal::$config['URL'] = 'http://'.$_SERVER['HTTP_HOST'].'/';
+}
+else {
+	Charcoal::$config['URL'] = 'http://localhost/';
+}
 
