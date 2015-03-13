3aab8ea8b39785cd44e574292971c0ee5ee3f2a8
Mathieu Ducharme <mat@locomotive.ca>
2015-03-13 14:06:35 -0400

    Clearer messages

--- setup_db.php
+++ setup_db.php
@@ -83,7 +83,7 @@ if(!isset($json['databases'])) {
 
 $databases = array_keys($json['databases']);
 if(in_array($db_ident, $databases)) {
-	$input = $climate->bold()->confirm('Database already exists in the config file. Overwrite?');
+	$input = $climate->bold()->confirm('Database ident already exists in the config file. Overwrite?');
 	if (!$input->confirmed()) {
 		$climate->red()->out('Setup aborted.');
 		die();
@@ -98,7 +98,7 @@ $json['databases'][$db_ident] = [
 
 $climate->green()->out('Database added successfully to config file.');
 
-$input = $climate->bold()->confirm(sprintf('Do you want to use "%s" as the default database?', $db_name));
+$input = $climate->bold()->confirm(sprintf('Do you want to use "%s" as the default database ident?', $db_name));
 if ($input->confirmed()) {
 	$json['default_database'] = $db_ident;
 }
