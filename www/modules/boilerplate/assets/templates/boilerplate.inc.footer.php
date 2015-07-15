222557aa94b8c5c5ab2212adca3d3defc4f23ecc
Antoine Boulanger <hello@antoineboulanger.com>
2015-07-15 18:15:41 -0400

    Update boilerplate.inc.footer.php
    scripts

--- www/modules/boilerplate/assets/templates/boilerplate.inc.footer.php
+++ www/modules/boilerplate/assets/templates/boilerplate.inc.footer.php
@@ -1,5 +1,6 @@
-		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-        <script>window.jQuery || document.write('<script src="modules/{{module}}/assets/scripts/src/vendors/jquery-1.11.2.min.js"><\/script>')</script>		
-        <!--<script src="modules/{{module}}/assets/scripts/dist/main.js"></script>-->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="modules/{{module}}/assets/scripts/dist/jquery-1.11.3.min.js"><\/script>')</script>
+        <script src="modules/{{module}}/assets/scripts/dist/vendors.js"></script>
+        <script src="modules/{{module}}/assets/scripts/dist/app.js"></script>
     </body>
 </html>
