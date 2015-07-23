7a9eedceb47c41f97a3208fb6e48afd55840beb9
Chauncey McAskill <chauncey@locomotive.ca>
2015-07-23 12:04:47 -0400

    Cleaned up configuration shared JSON and PHP loader.
    Changes:
    - Removed "local" database set;
    - Added `bower_components/` directory to accessible Charcoal\Assets;
    - Refactored config loader;
    - Replaced direct loading of Charcoal with Composer autoloader;
    - Added dynamic setting of "display_errors";
    - Added dynamic setting of timezone;
    - Added dynamic URL resolution.

--- config/config.php
+++ config/config.php
@@ -1,33 +1,86 @@
 <?php
+
 /**
-* config.php
-*/
+ * Charcoal Configuration
+ *
+ * @package Charcoal\Boilerplate
+ */
 
-// Error reporting
 error_reporting(E_ALL & ~E_STRICT);
-ini_set('display_errors', true);
 
+/**
+ * Import Charcoal
+ */
 
+include dirname(__DIR__) . '/www/charcoal/charcoal.php';
 
-// Main Charcoal include. This is where the Charcoal autoloader is defined.
-include __DIR__.'/../charcoal/charcoal.php';
+/**
+ * Configure Application
+ */
 
-// JSON Configuration
-Charcoal::add_config(__DIR__.'/config.json');
-$application_env = Charcoal::application_env();
-if(file_exists(__DIR__ . '/config.'.$application_env.'.json')) {
-	Charcoal::add_config(__DIR__ . '/config.'.$application_env.'.json');
+Charcoal::add_config(__DIR__ . '/config.json');
+
+$application_env   = Charcoal::application_env();
+$env_configuration = __DIR__ . "/config.{$application_env}.json";
+if (file_exists($env_configuration) ) {
+	Charcoal::add_config($env_configuration);
 }
 
-// Environment defaults
-date_default_timezone_set('America/Montreal');
+/**
+ * Setup Timezone
+ */
 
-// Configuration overwrite
-Charcoal::$config['ROOT'] = realpath(__DIR__).'/../';
-if(isset($_SERVER['HTTP_HOST'])) {
-	Charcoal::$config['URL'] = 'http://'.$_SERVER['HTTP_HOST'].'/';
-}
-else {
-	Charcoal::$config['URL'] = 'http://localhost/';
-}
+date_default_timezone_set( Charcoal::$config['default_timezone'] ?: 'America/Montreal' );
 
+/**
+ * Setup Debugging
+ */
+
+$debug_application = ( isset(Charcoal::$config['debug']['active']) && Charcoal::$config['debug']['active'] ? true : false );
+ini_set('display_errors', $debug_application);
+
+/**
+ * Resolve Locations
+ */
+
+Charcoal::$config['ROOT'] = rtrim( realpath( dirname(__DIR__) . '/www' ), '/\\' ) . '/';
+
+$php_sapi = php_sapi_name();
+
+if ( substr($php_sapi, 0, 3) !== 'cli' ) {
+	$name = ( Charcoal::$config['project_name'] ?: 'boilerplate' );
+	$http = ( Charcoal::$config['HTTP_MODE'] ?: '//' );
+	$host = ( getenv('HTTP_HOST') ?: 'localhost' );
+	$url  = '';
+
+	if ( empty(Charcoal::$config['project_url']) ) {
+		$url = $host;
+
+		$is_local = preg_match('#^(localhost|(127|192)\.\d{1,3}\.\d{1,3}\.\d{1,3})$#', $host);
+
+		/** Make an assumption about localhost environments */
+		if ( $is_local && false === strpos($url, $name) ) {
+			$url .= '/' . $name;
+		}
+	} else {
+		$search  = [ '#\[\[http|protocol|scheme\]\]#', '#\[\[host|server|domain\]\]#', '#\[\[(project|name)\]\]#' ];
+		$replace = [ $http, $host, $name ];
+
+		$url = preg_replace($search, $replace, Charcoal::$config['project_url']);
+	}
+
+	/** Clean up any typos and undesirables characters */
+	$url = preg_replace('|[^a-z0-9-~+_.?#=!&;,/:%@$\|*\'()\\x80-\\xff]|i', '', $url);
+	$url = str_replace(';//', '://', $url);
+
+	/**
+	 * If the URL doesn't appear to contain a scheme name, we must
+	 * presume it needs http:// prepended to the URL (unless it's
+	 * a relative link starting with /, # or ? or a PHP file).
+	 */
+	if ( strpos($url, ':') === false && ! in_array($url[0], [ '/', '#', '?' ]) && ! preg_match('/^[a-z0-9-]+?\.php/i', $url) ) {
+		$url = $http . $url;
+	}
+
+	Charcoal::$config['URL'] = rtrim($url, '/\\') . '/';
+}
