8a93c9d178d136144f04cad7cd85003c10ad520a
Chauncey McAskill <shimoroka@gmail.com>
2016-12-18 14:08:58 -0500

    Added ‘.has-no-js’ and ‘.has-js’ to <html>
    Useful for CSS, e.g.:
    
    ```
    .has-js .hide-if-js,
    .has-no-js .hide-if-no-js {
        display: none;
    }
    ```

--- assets/scripts/utils/environment.js
+++ assets/scripts/utils/environment.js
@@ -3,7 +3,7 @@ const DATA_API_KEY = '.data-api';
 
 const $document    = $(document);
 const $window      = $(window);
-const $html        = $(document.documentElement);
+const $html        = $(document.documentElement).removeClass('has-no-js').addClass('has-js');
 const $body        = $(document.body);
 
 export { $document, $window, $html, $body, APP_NAME, DATA_API_KEY };
