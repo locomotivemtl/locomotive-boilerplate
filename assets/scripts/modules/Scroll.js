61c9c0ac6f2c92feec06a54fa5978dd7924e5d03
Jérémy Minié <jeremy.minie@gmail.com>
2019-09-25 16:25:14 -0400

    Add basic scroll-related lazy loading system w/ examples + Fix scrolling issues by putting header inside `o-scroll` + Use `data-scroll-section`

--- assets/scripts/modules/Scroll.js
+++ assets/scripts/modules/Scroll.js
@@ -11,6 +11,27 @@ export default class extends module {
             el: this.el,
             smooth: true
         });
+
+        this.scroll.on('call', (func,way,obj,id) => {
+            // Using modularJS
+            this.call(func[0],{way,obj},func[1],func[2]);
+        });
+
+        this.scroll.on('scroll', (args) => {
+            // console.log(args.scroll);
+        })
+    }
+
+    toggleLazy(args) {
+        let src = this.getData('lazy', args.obj.el)
+        if(src.length) {
+            if(args.obj.el.tagName == "IMG") {
+                args.obj.el.src = src
+            } else {
+                args.obj.el.style.backgroundImage = `url(${src})`
+            }
+            this.setData('lazy', '', args.obj.el)
+        }
     }
 
     destroy() {
