e686689b5243cf9f5f7de226e8ce7121aa528ea4
Quentin Hocdé <quentinhcd@gmail.com>
2020-11-25 15:10:53 -0500

    Add shuffle function in array utils

--- assets/scripts/utils/array.js
+++ assets/scripts/utils/array.js
@@ -86,3 +86,16 @@ export function findByKeyValue( array, key, value ) {
 export function cloneArray( array ) {
     return JSON.parse(JSON.stringify(array));
 }
+
+/**
+ * Shuffles array in place. ES6 version
+ * @param {Array} a items An array containing the items.
+ */
+export function shuffle(a) {
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+}
+
