f52748846464f6195c0b0a67d5accd349322c182
Lucas Vallenet <lucasvallenet@gmail.com>
2022-06-02 17:23:06 +0200

    Delete queryClosestParent function

--- assets/scripts/utils/html.js
+++ assets/scripts/utils/html.js
@@ -131,47 +131,10 @@ const getParents = $el => {
 }
 
 
-/**
- * Find every closest parent to element
- * @param  {HTMLElement}    $el     - DOM Element
- * @param  {string}         select  - parent elements seelctor
- * @return {array}          parent nodes
- */
-
-const queryClosestParent = ($el, selector) => {
-
-    // Element.matches() polyfill
-    if (!Element.prototype.matches) {
-        Element.prototype.matches =
-            Element.prototype.matchesSelector ||
-            Element.prototype.mozMatchesSelector ||
-            Element.prototype.msMatchesSelector ||
-            Element.prototype.oMatchesSelector ||
-            Element.prototype.webkitMatchesSelector ||
-            function(s) {
-                const matches = (this.document || this.ownerDocument).querySelectorAll(s)
-                const i = matches.length
-                while (--i >= 0 && matches.item(i) !== this) {}
-                return i > -1
-            }
-    }
-
-    // Get the closest matching element
-    for (; $el && $el !== document; $el = $el.parentNode) {
-        if ($el.matches(selector)) {
-            return $el
-        }
-    }
-
-    return null
-}
-
-
 export {
     escapeHtml,
     unescapeHtml,
     getNodeData,
     getData,
     getParents,
-    queryClosestParent
 }
