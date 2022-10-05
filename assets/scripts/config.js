feb22411645eeaa4d7491e31e1094f00c2d7fe26
arnvvd <pinot.arnaud@gmail.com>
2022-10-05 16:39:25 -0400

    Add documentation for esbuild process.env

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -1,3 +1,12 @@
+/**
+ * > When using the esBuild API, all `process.env.NODE_ENV` expressions
+ * > are automatically defined to `"production"` if all minification
+ * > options are enabled and `"development"` otherwise. This only happens
+ * > if `process`, `process.env`, and `process.env.NODE_ENV` are not already
+ * > defined. This substitution is necessary to avoid code crashing instantly
+ * > (since `process` is a Node API, not a web API).
+ * > — https://esbuild.github.io/api/#platform
+ */
 const env = process.env.NODE_ENV
 
 export default config = Object.freeze({
