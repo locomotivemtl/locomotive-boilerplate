8f3034d54a7b032cccf9799f9a62a1e477a881b7
Deven Caron <devencaron@gmail.com>
2023-02-01 09:49:25 -0500

    Run precommit

--- assets/scripts/utils/transform.js
+++ assets/scripts/utils/transform.js
@@ -4,32 +4,32 @@
  * @return {number|object}  translate value
  */
 
-const getTranslate = $el => {
-
-    if(!window.getComputedStyle) {
+const getTranslate = ($el) => {
+    if (!window.getComputedStyle) {
         return
     }
 
     let translate
     const style = getComputedStyle($el)
-    const transform = style.msTransform || style.webkitTransform || style.MozTransform || style.OTransform || style.transform
+    const transform =
+        style.msTransform ||
+        style.webkitTransform ||
+        style.MozTransform ||
+        style.OTransform ||
+        style.transform
 
     const matrix3D = transform.match(/^matrix3d\((.+)\)$/)
-    if(matrix3D) {
+    if (matrix3D) {
         translate = parseFloat(matrix3D[1].split(', ')[13])
     } else {
         const matrix = transform.match(/^matrix\((.+)\)$/)
         translate = {
             x: matrix ? parseFloat(matrix[1].split(', ')[4]) : 0,
-            y: matrix ? parseFloat(matrix[1].split(', ')[5]) : 0
+            y: matrix ? parseFloat(matrix[1].split(', ')[5]) : 0,
         }
     }
 
     return translate
 }
 
-
-export {
-    transform,
-    getTranslate
-}
+export { transform, getTranslate }
